fe05d15376d116836bd7a2bce23d8086878333b6
Tiago Marques de Oliveira <105756442+tiagoKurt@users.noreply.github.com>
2023-09-11 11:49:12 -0300

    mais mudanças

--- arvoresAVL/arvoresAVL/src/arvores/avl_binaria/FreqPalavras.java
+++ arvoresAVL/arvoresAVL/src/arvores/avl_binaria/FreqPalavras.java
@@ -11,7 +11,41 @@ public class FreqPalavras {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        String nomeArquivo = "./src/dicTXT/teste.txt"; 
+        String nomeArquivo = "./src/arvores/arquivos/teste.txt"; 
+        Map<String, Integer> mapaPalavras = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                linha = linha.toLowerCase().replaceAll("[^a-zA-ZÀ-ÿ\\s]", " ");
+                String[] palavras = linha.split("\\s+");
+
+                for (String palavra : palavras) {
+                    if (!palavra.isEmpty()) {
+                        mapaPalavras.put(palavra, mapaPalavras.getOrDefault(palavra, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Map<String, Integer> mapaOrdenado = new TreeMap<>(Collator.getInstance());
+        mapaOrdenado.putAll(mapaPalavras);
+
+        for (Map.Entry<String, Integer> entry : mapaOrdenado.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        System.out.println("Tempo de execução: " + executionTime + " milissegundos");
+    }
+    
+    public void frequenciaPalavras(String nomeArquivo){
+        long startTime = System.currentTimeMillis();
+
+        nomeArquivo = "./src/dicTXT/teste.txt"; 
         Map<String, Integer> mapaPalavras = new HashMap<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
--- arvoresAVL/arvoresAVL/src/arvores/visao/principal.java
+++ arvoresAVL/arvoresAVL/src/arvores/visao/principal.java
@@ -1,18 +1,22 @@
 package arvores.visao;
 
+import Arvores.avl_binaria.FreqPalavras;
 import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import javax.swing.JFileChooser;
-
+import javax.swing.JOptionPane;
 
 public class principal extends javax.swing.JFrame {
 
-    
     public principal() {
         initComponents();
-        
+
         setLocationRelativeTo(null);
+        jTextField1_nomeDoArquivo.setVisible(false);
+        jButton1_frequencia.setVisible(false);
     }
 
     /**
@@ -24,22 +28,51 @@ public class principal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1_limparCampos = new javax.swing.JButton();
+        jButton1_frequencia = new javax.swing.JButton();
         jButton1_BuscarTexto = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jTextField1_nomeDoArquivo = new javax.swing.JTextField();
+        jButton1_BuscarTexto1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jButton1_limparCampos.setFont(new java.awt.Font("Serif", 3, 16)); // NOI18N
+        jButton1_limparCampos.setText("LIMPAR CAMPOS");
+        jButton1_limparCampos.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 3));
+        jButton1_limparCampos.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1_limparCampos.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1_limparCamposActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1_limparCampos, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 650, 160, 40));
+
+        jButton1_frequencia.setFont(new java.awt.Font("Serif", 3, 16)); // NOI18N
+        jButton1_frequencia.setText("FREQUÊNCIA");
+        jButton1_frequencia.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 3));
+        jButton1_frequencia.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1_frequencia.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1_frequenciaActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1_frequencia, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 270, 160, 40));
+
+        jButton1_BuscarTexto.setFont(new java.awt.Font("Serif", 3, 16)); // NOI18N
         jButton1_BuscarTexto.setText("BUSCAR TEXTO");
+        jButton1_BuscarTexto.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 3));
+        jButton1_BuscarTexto.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButton1_BuscarTexto.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1_BuscarTextoActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1_BuscarTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(640, 210, 130, 40));
+        getContentPane().add(jButton1_BuscarTexto, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 220, 160, 40));
 
         jLabel2.setFont(new java.awt.Font("Serif", 3, 36)); // NOI18N
         jLabel2.setText("BUSCA EM ÁRVORES");
@@ -48,7 +81,23 @@ public class principal extends javax.swing.JFrame {
         jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/arvores/icons/floresta.png"))); // NOI18N
         getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 40, 90, 70));
 
+        jTextField1_nomeDoArquivo.setFont(new java.awt.Font("Serif", 3, 14)); // NOI18N
+        jTextField1_nomeDoArquivo.setBorder(javax.swing.BorderFactory.createTitledBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 3), "ARQUIVO SELECIONADO", javax.swing.border.TitledBorder.RIGHT, javax.swing.border.TitledBorder.TOP, new java.awt.Font("Bodoni MT", 3, 15))); // NOI18N
+        getContentPane().add(jTextField1_nomeDoArquivo, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 220, 500, 50));
+
+        jButton1_BuscarTexto1.setFont(new java.awt.Font("Serif", 3, 16)); // NOI18N
+        jButton1_BuscarTexto1.setText("SAIR");
+        jButton1_BuscarTexto1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 3));
+        jButton1_BuscarTexto1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        jButton1_BuscarTexto1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1_BuscarTexto1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1_BuscarTexto1, new org.netbeans.lib.awtextra.AbsoluteConstraints(700, 710, 160, 40));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/arvores/icons/5479107.jpg"))); // NOI18N
+        jLabel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0), 4));
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1110, 940));
 
         pack();
@@ -56,23 +105,54 @@ public class principal extends javax.swing.JFrame {
 
     private void jButton1_BuscarTextoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1_BuscarTextoActionPerformed
         try {
-            JFileChooser fc = new JFileChooser("./src/arvores.arquivos");
-            File buscar = new File(".src/arvores.arquivos");
+
+            JFileChooser fc = new JFileChooser("./src/arvores/arquivos");
+            File buscar = new File(".src/arvores/arquivos");
 
             fc.setCurrentDirectory(buscar);
             fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
             fc.showOpenDialog(this);
             File arquivo = fc.getSelectedFile();
             String nomeDoArquivo = arquivo.getPath();
-            jTextField1_urlImagens.setText(nomeDoArquivo);
-            ImageIcon iconLogo = new ImageIcon(nomeDoArquivo);
-            iconLogo.setImage(iconLogo.getImage().getScaledInstance(ImagensMarcas.getWidth(), ImagensMarcas.getHeight(), 1));
-            ImagensMarcas.setIcon(iconLogo);
-            ImagensMarcas1.setVisible(false);
+            jTextField1_nomeDoArquivo.setText(nomeDoArquivo);
+            jTextField1_nomeDoArquivo.setVisible(true);
+            jButton1_frequencia.setVisible(true);
+
         } catch (Exception erro) {
             JOptionPane.showMessageDialog(this, "Arquivo não selecionado!");
         }
     }//GEN-LAST:event_jButton1_BuscarTextoActionPerformed
+
+    private void jButton1_BuscarTexto1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1_BuscarTexto1ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton1_BuscarTexto1ActionPerformed
+
+    private void jButton1_frequenciaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1_frequenciaActionPerformed
+        FreqPalavras pog = new FreqPalavras();
+        String caminhoCortado = "";
+        
+        String caminhoCompleto = jTextField1_nomeDoArquivo.getText(); // Substitua pelo seu caminho completo
+        String caminhoDesejado = "src";
+
+        Path path = Paths.get(caminhoCompleto);
+
+        // Encontre o índice onde "SRC" começa no caminho completo
+        int startIndex = caminhoCompleto.indexOf(caminhoDesejado);
+
+        if (startIndex != -1) {
+            // Extraia a parte do caminho a partir do índice "SRC" para frente
+            caminhoCortado = caminhoCompleto.substring(startIndex);
+        }
+        
+        pog.frequenciaPalavras(caminhoCortado);
+    }//GEN-LAST:event_jButton1_frequenciaActionPerformed
+
+    private void jButton1_limparCamposActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1_limparCamposActionPerformed
+        jTextField1_nomeDoArquivo.setText("");
+        jButton1_frequencia.setVisible(false);
+        jTextField1_nomeDoArquivo.setVisible(false);
+
+    }//GEN-LAST:event_jButton1_limparCamposActionPerformed
 
     /**
      * @param args the command line arguments
@@ -111,8 +191,12 @@ public class principal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1_BuscarTexto;
+    private javax.swing.JButton jButton1_BuscarTexto1;
+    private javax.swing.JButton jButton1_frequencia;
+    private javax.swing.JButton jButton1_limparCampos;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JTextField jTextField1_nomeDoArquivo;
     // End of variables declaration//GEN-END:variables
 }
